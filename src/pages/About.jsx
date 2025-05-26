0d00af164fe2f404f18b024332263ae07528a087
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-26 12:12:08 +0200

    init

--- src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="text-center mt-20">
-      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind 👋</h1>
-    </div>
-  );
-}
---
+++ src/pages/home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function home() {
+  return <div className="bg-red-500 text-4xl">Hello home!</div>;
+}
