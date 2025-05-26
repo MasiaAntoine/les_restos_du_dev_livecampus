a871c98140be084ee2b88a374a119d11ddfb1a7e
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-26 17:09:50 +0200

    correction firebase import

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,46 +1,44 @@
-import { initializeApp } from 'firebase/app';
-import type { FirebaseApp } from 'firebase/app';
-import type { DocumentData, DocumentSnapshot } from 'firebase/firestore';
-import { Firestore, getFirestore, doc, getDoc } from 'firebase/firestore';
-import type { Auth, UserCredential } from 'firebase/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { firebaseConfig } from 'firebase.config.ts';
+import { initializeApp } from 'firebase/app'
+import type { FirebaseApp } from 'firebase/app'
+import type { DocumentData, DocumentSnapshot } from 'firebase/firestore'
+import { Firestore, getFirestore, doc, getDoc } from 'firebase/firestore'
+import type { Auth, UserCredential } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { firebaseConfig } from '/firebase.config'
 
-let firebaseApp: FirebaseApp | undefined;
-let firestore: Firestore | undefined;
-let fireAuth: Auth | undefined;
+let firebaseApp: FirebaseApp | undefined
+let firestore: Firestore | undefined
+let fireAuth: Auth | undefined
 
 export function getApp(): FirebaseApp {
-  firebaseApp = firebaseApp ? firebaseApp : initializeApp(firebaseConfig);
+  firebaseApp = firebaseApp ? firebaseApp : initializeApp(firebaseConfig)
 
-  return firebaseApp;
+  return firebaseApp
 }
 
 export function getFs(): Firestore {
-  firestore = firestore ? firestore : getFirestore(getApp());
+  firestore = firestore ? firestore : getFirestore(getApp())
 
-  return firestore;
+  return firestore
 }
 
 export function getFireAuth(): Auth {
-  fireAuth = fireAuth ? fireAuth : getAuth(getApp());
+  fireAuth = fireAuth ? fireAuth : getAuth(getApp())
 
-  return fireAuth;
+  return fireAuth
 }
 
 export function signIn(
   email: string,
-  password: string,
+  password: string
 ): Promise<UserCredential> {
-  return signInWithEmailAndPassword(getFireAuth(), email, password);
+  return signInWithEmailAndPassword(getFireAuth(), email, password)
 }
 
 export async function getDocument<T extends DocumentData = DocumentData>(
-  path: string,
+  path: string
 ): Promise<T | undefined> {
-  const docSnapshot: DocumentSnapshot = await getDoc(
-    doc(getFirestore(), path),
-  );
+  const docSnapshot: DocumentSnapshot = await getDoc(doc(getFirestore(), path))
 
-  return docSnapshot.data() as T;
+  return docSnapshot.data() as T
 }
