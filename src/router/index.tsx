e4fd0d9df4208e8514538adf18311a008b808393
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-26 14:40:52 +0200

    Ajout des composants Tabs et mise à jour de la page de profil pour inclure les onglets

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/home";
 import Register from "../pages/register";
 import Login from "../pages/login";
 import ForgotPassword from "../pages/forgot-password";
+import Profile from "../pages/profile";
 
 export default function index() {
   return (
@@ -13,6 +14,7 @@ export default function index() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
