2c1ce6187c01f16785b269baf0f283e1b285ffb6
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-26 12:15:59 +0200

    init shadcn

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 export default function home() {
-  return <div className="bg-red-500 text-4xl">Hello home!</div>;
+  return (
+    <>
+      <div className="bg-red-500 text-4xl">Hello home!</div>
+      <Button>Click me</Button>
+    </>
+  );
 }
