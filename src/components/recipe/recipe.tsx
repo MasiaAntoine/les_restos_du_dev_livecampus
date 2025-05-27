90efdfa9e51abbb93ba3bf1321e11581df78a82a
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-27 22:57:42 +0200

    Refactorisation du composant Recipe pour améliorer la lisibilité du code et ajout d'un bouton pour afficher les détails des recettes.

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import RecipeCard from './card-recipe';
-import AddRecipe from './add-recipe';
+import { useContext, useEffect, useState } from 'react'
+import RecipeCard from './card-recipe'
+import AddRecipe from './add-recipe'
 
 import {
   Card,
@@ -9,52 +9,52 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { ServicesContext } from '@/contexts/contexts.tsx';
-import type { RecipeModel } from '@/models/Recipe.model.ts';
+} from '@/components/ui/card'
+import { ServicesContext } from '@/contexts/contexts.tsx'
+import type { RecipeModel } from '@/models/Recipe.model.ts'
 
 export default function RecipeComponent() {
-  const [recipesList, setRecipesList] = useState<RecipeModel[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const services = useContext(ServicesContext);
-  const currentUser = services?.currentUser;
-  const recipesService = services?.recipesService;
+  const [recipesList, setRecipesList] = useState<RecipeModel[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const services = useContext(ServicesContext)
+  const currentUser = services?.currentUser
+  const recipesService = services?.recipesService
 
   useEffect(() => {
     if (services && recipesService && currentUser && currentUser.displayName) {
-      setIsLoading(true);
-      recipesService.getRecipesByAuthor(currentUser.displayName)
-        .then((recipes: RecipeModel[]) => setRecipesList(recipes));
+      setIsLoading(true)
+      recipesService
+        .getRecipesByAuthor(currentUser.displayName)
+        .then((recipes: RecipeModel[]) => setRecipesList(recipes))
     }
-  }, [services, recipesService, currentUser]);
+  }, [services, recipesService, currentUser])
 
   const handleDeleteRecipe = (id: string) => {
-    setRecipesList(recipesList.filter((recipe) => recipe.id !== id));
-    console.log('appeler l\'API pour supprimer la recette avec l\'ID:', id);
-  };
+    setRecipesList(recipesList.filter((recipe) => recipe.id !== id))
+    console.log("appeler l'API pour supprimer la recette avec l'ID:", id)
+  }
 
   const handleAddRecipe = (newRecipe: RecipeModel) => {
-    setIsLoading(true);
+    setIsLoading(true)
     if (!services || !recipesService) {
-      throw new Error('Services or recipesService not found');
+      throw new Error('Services or recipesService not found')
     }
     if (!newRecipe) {
-      throw new Error('Object recipe is empty or undefined');
+      throw new Error('Object recipe is empty or undefined')
     }
-    setRecipesList([...recipesList, newRecipe]);
+    setRecipesList([...recipesList, newRecipe])
 
-    recipesService.createRecipe(newRecipe)
-      .then(() => setIsLoading(false));
-  };
+    recipesService.createRecipe(newRecipe).then(() => setIsLoading(false))
+  }
 
   const handleEditRecipe = (id: string, updatedRecipe: object) => {
     setRecipesList(
       recipesList.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe,
-      ),
-    );
-    console.log('appeler l\'API Recette mise à jour:', updatedRecipe, isLoading);
-  };
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      )
+    )
+    console.log("appeler l'API Recette mise à jour:", updatedRecipe, isLoading)
+  }
 
   return (
     <>
@@ -65,7 +65,7 @@ export default function RecipeComponent() {
         </CardHeader>
         <CardContent>
           <div className="mb-6">
-            <AddRecipe onRecipeAdd={handleAddRecipe}/>
+            <AddRecipe onRecipeAdd={handleAddRecipe} />
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -75,6 +75,7 @@ export default function RecipeComponent() {
                 recipe={recipe}
                 onDelete={handleDeleteRecipe}
                 onEdit={handleEditRecipe}
+                showDetailsButton={true}
               />
             ))}
           </div>
@@ -84,5 +85,5 @@ export default function RecipeComponent() {
         </CardFooter>
       </Card>
     </>
-  );
+  )
 }
