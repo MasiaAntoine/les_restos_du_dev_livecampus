3892575b67e5dace59a44a839ec497982b459a20
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-27 22:11:09 +0200

    Ajout du composant Toaster avec intégration de sonner et gestion des thèmes. Mise à jour de package.json pour inclure les dépendances next-themes et sonner.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
-import { useMemo, useState } from 'react';
-import Router from './router';
-import { FirebaseService } from '@/services/firebase/firebase.service.tsx';
-import { ServicesContext } from './contexts/contexts.tsx';
-import { AuthService } from '@/services/firebase/auth.service.tsx';
-import { UserService } from '@/services/firebase/user.service.tsx';
-import { IngredientsService } from '@/services/firebase/ingredients.service.ts';
-import { RecipesService } from '@/services/firebase/recipes.service.ts';
-import type { UserModel } from '@/models/User.model.ts';
+import { useMemo, useState } from 'react'
+import Router from './router'
+import { FirebaseService } from '@/services/firebase/firebase.service.tsx'
+import { ServicesContext } from './contexts/contexts.tsx'
+import { AuthService } from '@/services/firebase/auth.service.tsx'
+import { UserService } from '@/services/firebase/user.service.tsx'
+import { IngredientsService } from '@/services/firebase/ingredients.service.ts'
+import { RecipesService } from '@/services/firebase/recipes.service.ts'
+import type { UserModel } from '@/models/User.model.ts'
+import { Toaster } from '@/components/ui/sonner'
 
 function App() {
-  const [currentUser, setCurrentUser] = useState<UserModel | null>(null);
-  const firebaseService: FirebaseService = useMemo(() => new FirebaseService(), []);
-  const userService: UserService = useMemo(() => new UserService(firebaseService), [firebaseService]);
-  const authService: AuthService = useMemo(() => new AuthService(firebaseService, setCurrentUser, userService), [firebaseService, userService]);
-  const ingredientsService = useMemo(() => new IngredientsService(firebaseService), [firebaseService]);
-  const recipesService = useMemo(() => new RecipesService(firebaseService), [firebaseService]);
+  const [currentUser, setCurrentUser] = useState<UserModel | null>(null)
+  const firebaseService: FirebaseService = useMemo(
+    () => new FirebaseService(),
+    []
+  )
+  const userService: UserService = useMemo(
+    () => new UserService(firebaseService),
+    [firebaseService]
+  )
+  const authService: AuthService = useMemo(
+    () => new AuthService(firebaseService, setCurrentUser, userService),
+    [firebaseService, userService]
+  )
+  const ingredientsService = useMemo(
+    () => new IngredientsService(firebaseService),
+    [firebaseService]
+  )
+  const recipesService = useMemo(
+    () => new RecipesService(firebaseService),
+    [firebaseService]
+  )
 
   return (
-    <ServicesContext.Provider value={{
-      authService: authService,
-      userService: userService,
-      ingredientsService: ingredientsService,
-      recipesService: recipesService,
-      currentUser: currentUser,
-    }}>
-      <Router/>
+    <ServicesContext.Provider
+      value={{
+        authService: authService,
+        userService: userService,
+        ingredientsService: ingredientsService,
+        recipesService: recipesService,
+        currentUser: currentUser,
+      }}
+    >
+      <Router />
+      <Toaster />
     </ServicesContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
