3f6c9ee62047ba3dea036a21cf4cc82d18b7b880
ThorKild_ <pro.antoine.masia@gmail.com>
2025-05-27 11:33:03 +0200

    Réorganisation de la structure du composant MenuProfile pour intégrer l'image du logo dans le conteneur des onglets.

--- src/pages/profile-menu.tsx
+++ src/pages/profile-menu.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import ProfileComponent from '@/components/profile/profile'
 import RecipeComponent from '@/components/recipe/recipe'
+import logo from '@/assets/logo.png'
 
 export default function MenuProfileComponent() {
   return (
     <>
       <div className="flex p-10 justify-center h-screen w-screen">
         <Tabs defaultValue="recipe" className="w-full">
-          <TabsList>
-            <TabsTrigger value="recipe">Recette</TabsTrigger>{' '}
-            <TabsTrigger value="info-profile">Profile</TabsTrigger>
-          </TabsList>
+          <div className="flex items-end justify-between">
+            <TabsList>
+              <TabsTrigger value="recipe">Recette</TabsTrigger>{' '}
+              <TabsTrigger value="info-profile">Profile</TabsTrigger>
+            </TabsList>
+
+            <img
+              src={logo}
+              alt="Les Restos du Dev"
+              className="size-26 rounded-full"
+            />
+          </div>
 
           <TabsContent value="recipe">
             <RecipeComponent />
