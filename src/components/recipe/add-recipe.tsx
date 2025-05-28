cf78b2cf9ffe6b1a83a25483d0582aacf817d56b
TristanLBD <lbdtristan@gmail.com>
2025-05-28 14:24:15 +0200

    Mise a jour de l'image par defaut des recettes

--- src/components/recipe/add-recipe.tsx
+++ src/components/recipe/add-recipe.tsx
@@ -1,6 +1,6 @@
-'use client';
+'use client'
 
-import { useContext, useState } from 'react';
+import { useContext, useState } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -8,36 +8,37 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import { Button } from '../ui/button';
-import { ServicesContext } from '@/contexts/contexts.tsx';
-import type { RecipeModel } from '@/models/Recipe.model.ts';
-import { v4 as uuid } from 'uuid';
-import RecipeForm from './recipe-form';
-import type { FormData } from '@/components/recipe/recipe-form.constants.ts';
+} from '@/components/ui/dialog'
+import { Button } from '../ui/button'
+import { ServicesContext } from '@/contexts/contexts.tsx'
+import type { RecipeModel } from '@/models/Recipe.model.ts'
+import { v4 as uuid } from 'uuid'
+import RecipeForm from './recipe-form'
+import type { FormData } from '@/components/recipe/recipe-form.constants.ts'
 
 export default function AddRecipeComponent({
-                                             onRecipeAdd,
-                                           }: {
+  onRecipeAdd,
+}: {
   onRecipeAdd: (recipe: RecipeModel) => void
 }) {
-  const services = useContext(ServicesContext);
-  const currentUser = services?.currentUser;
-  const [open, setOpen] = useState(false);
+  const services = useContext(ServicesContext)
+  const currentUser = services?.currentUser
+  const [open, setOpen] = useState(false)
 
   const handleSubmit = (values: FormData) => {
     console.log(
       'AddRecipe handleSubmit appelé avec:',
-      JSON.stringify(values, null, 2),
-    );
-    const recipeId = uuid();
+      JSON.stringify(values, null, 2)
+    )
+    const recipeId = uuid()
     try {
       const newRecipe: RecipeModel = {
         id: recipeId,
         title: values.name || 'Sans titre',
         cookTime: `${values.preparationTime || '0'} minutes`,
         author: currentUser?.displayName || 'Anonyme',
-        imageUrl: 'https://via.placeholder.com/150',
+        imageUrl:
+          'https://www.lunariarecruitment.co.uk/wp-content/uploads/sites/93/2013/11/dummy-image-square.jpg',
         ingredients: values.ingredients.map((ing) => ({
           ...ing,
           ingredientId: ing.ingredientId || '',
@@ -45,17 +46,17 @@ export default function AddRecipeComponent({
           quantity: ing.quantity || 0,
           unit: ing.unit || '',
         })),
-      };
+      }
       console.log(
         'Nouvelle recette (nettoyée):',
-        JSON.stringify(newRecipe, null, 2),
-      );
-      onRecipeAdd(newRecipe);
-      setOpen(false);
+        JSON.stringify(newRecipe, null, 2)
+      )
+      onRecipeAdd(newRecipe)
+      setOpen(false)
     } catch (error) {
-      console.error('Erreur lors de la création de la recette:', error);
+      console.error('Erreur lors de la création de la recette:', error)
     }
-  };
+  }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -69,8 +70,8 @@ export default function AddRecipeComponent({
             Remplissez le formulaire pour ajouter une nouvelle recette.
           </DialogDescription>
         </DialogHeader>
-        <RecipeForm onSubmit={handleSubmit}/>
+        <RecipeForm onSubmit={handleSubmit} />
       </DialogContent>
     </Dialog>
-  );
+  )
 }
